991eb5305bd575bf4aa0255af72b856008095f9e
makia <g1821120@gmail.com>
2019-12-23 14:25:01 +0900

    コンストラクタを作成完了

--- Game/Game/src/Kyokumen.cpp
+++ Game/Game/src/Kyokumen.cpp
@@ -1,1 +1,31 @@
 #include "Kyokumen.hpp"
+
+Kyokumen::Kyokumen(const uint32 tesu_, const array<array<uint32, 9>, 9>& board_, const array<uint32, 41>& motigoma_)
+: m_kingSelfPos(0)
+, m_kingEnemyPos(0)
+, m_tesu(tesu_)
+, m_value(0) {
+    m_ban.fill(Wall);
+
+    for (uint32 dan{1}; dan <= 9; ++dan) {
+        for (uint32 suji{1}; suji <= 9; ++suji) {
+            m_ban[suji + dan] = board_[dan - 1][9 - suji];
+
+            if (m_ban[suji + dan] == Sou) {
+                m_kingSelfPos = suji + dan;
+            }
+            if (m_ban[suji + dan] == Eou) {
+                m_kingEnemyPos = suji + dan;
+            }
+
+            m_value += KomaValue[m_ban[suji + dan]];
+        }
+    }
+
+    for (uint32 i{}; i <= Eou; ++i) {
+        m_holdingKomas[i] = motigoma_[i];
+        m_value += HandValue[i] * m_holdingKomas[i];
+    }
+
+    InitControl();
+}
