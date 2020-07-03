27e3857a7fcbbdeb2a64a034c643ff962fb4703d
makia <g1821120@gmail.com>
2020-07-03 11:33:07 +0900

    Photon適用完了

--- Shogi/src/Common.hpp
+++ Shogi/src/Common.hpp
@@ -1,48 +1,41 @@
 ﻿
-# pragma once
+#pragma once
 #include <Siv3D.hpp>
 #include "Board.hpp"
 using std::array;
 
 // シーンの名前
 enum class State {
-    Title,  // タイトルシーン
-    Select,	// 設定シーン
-    Game,   // 二人用プレイのシーン
-    GameAI  // 一人用プレイのシーン
+    Title,   // タイトルシーン
+    Select,  // 設定シーン
+    Game,    // 二人用プレイのシーン
+    GameAI   // 一人用プレイのシーン
 };
 
 /// <summary>
 /// ターン
 /// </summary>
-enum class Turn {
-    Player,
-    Enemy,
-    Tsumi
-};
+enum class Turn { Player, Enemy, Tsumi };
 
 /// <summary>
 /// 駒落ち
 /// </summary>
 enum class Handicap {
-    Hirate,	// 平手
-    Bishop,	// 角落ち
-    Rook,	// 飛車落ち
-    TwoPieces,	// 2枚落ち
-    FourDrops,	// 4枚落ち
-    SixDrops,	// 6枚落ち
-    EightDrops,	// 8枚落ち
-    TenDrops,	// 10枚落ち
-    Custom	// カスタム
+    Hirate,      // 平手
+    Bishop,      // 角落ち
+    Rook,        // 飛車落ち
+    TwoPieces,   // 2枚落ち
+    FourDrops,   // 4枚落ち
+    SixDrops,    // 6枚落ち
+    EightDrops,  // 8枚落ち
+    TenDrops,    // 10枚落ち
+    Custom       // カスタム
 };
 
 /// <summary>
 /// 駒落ちする方
 /// </summary>
-enum class Elegance {
-    Player,
-    Enemy
-};
+enum class Elegance { Player, Enemy };
 
 /// <summary>
 /// ゲームデータ
@@ -159,11 +152,8 @@ public:
 /// </summary>
 using MyApp = SceneManager<State, GameData>;
 
+
 /// <summary>
 /// 勝者
 /// </summary>
-enum class Winner {
-    Player,
-    Enemy,
-    Sennitite
-};
+enum class Winner { Player, Enemy, Sennitite };
